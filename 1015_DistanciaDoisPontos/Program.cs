d8262db1ecd41e44e7e18de94a5bd351a902f486
Thais Fernandes Martins <113777376+ThaisMartins18@users.noreply.github.com>
2023-03-16 17:13:12 -0300

    Add files via upload
    16/03

---
+++ 1015_DistanciaDoisPontos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+ 
+class URI {
+    
+    static void Main(string[] args) { 
+
+        string[] ponto1 = Console.ReadLine().Split(' ');
+        double x1 = double.Parse(ponto1[0], CultureInfo.InvariantCulture);
+        double y1 = double.Parse(ponto1[1], CultureInfo.InvariantCulture);
+        
+        string[] ponto2 = Console.ReadLine().Split(' ');
+        double x2 = double.Parse(ponto2[0], CultureInfo.InvariantCulture);
+        double y2 = double.Parse(ponto2[1], CultureInfo.InvariantCulture);
+
+        double Distancia = Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2);
+        Distancia = Math.Sqrt(Distancia);
+
+        Console.WriteLine($"{Distancia.ToString("F4", CultureInfo.InvariantCulture)}");
+
+        Console.ReadKey();
+    }
+}
---
+++ 1017_GastoCombustivel/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int TempoViagem = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
+        int VelocidadeMedia = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
+        
+        double QtdLitros = Convert.ToDouble(VelocidadeMedia * TempoViagem) / 12;
+
+        Console.WriteLine($"{QtdLitros.ToString("F3", CultureInfo.InvariantCulture)}");
+
+        Console.ReadKey();
+    }
+}
