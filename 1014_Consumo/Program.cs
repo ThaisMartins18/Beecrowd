d12328695d12bef7ef8682fd219173063b094df2
Thais Fernandes Martins <113777376+ThaisMartins18@users.noreply.github.com>
2023-03-16 16:43:31 -0300

    Add files via upload

---
+++ 1014_Consumo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int X = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double Y = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double Consumo = X / Y;
+
+        Console.WriteLine($"{Consumo.ToString("F3", CultureInfo.InvariantCulture)} km/l");
+
+        Console.ReadKey();
+        
+    }
+
+}
---
+++ 1016_Distancia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int X = Convert.ToInt32(Console.ReadLine());
+        
+        double Tempo = (X * 2);
+
+        Console.WriteLine($"{Tempo} minutos");
+
+        Console.ReadKey();
+    }
+}
