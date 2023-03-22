debda90a27868463300e2f16d2ee181ff97c8b29
Thais Fernandes Martins <113777376+ThaisMartins18@users.noreply.github.com>
2023-03-22 15:53:36 -0300

    Add files via upload

---
+++ 1018_Cedulas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int [] notas = new int[7] { 100, 50, 20, 10, 5, 2, 1};
+
+        int valor = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(valor);
+
+        for (int i = 0; i < notas.Length; i++)
+        {
+            Console.WriteLine(valor / notas[i] + " nota(s) de R$ " + notas[i] + ",00");
+
+            valor = (valor%notas[i]);
+        }
+   
+        Console.ReadKey();
+    }
+}
