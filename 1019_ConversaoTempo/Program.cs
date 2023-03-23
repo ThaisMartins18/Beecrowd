f0bb3866e498d2d67386fae3d86ce2ec901fbd86
Thais Fernandes Martins <113777376+ThaisMartins18@users.noreply.github.com>
2023-03-23 15:27:53 -0300

    Add files via upload
    23/03

---
+++ 1019_ConversaoTempo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+
+        int segundos = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{segundos/3600}:{(segundos%3600)/60}:{segundos%60}");
+
+        Console.ReadKey();
+    }
+}
+
+
+
+
---
+++ 1020_IdadeDias/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int idadeDias = Convert.ToInt32(Console.ReadLine());
+
+        int ano = idadeDias/365;
+        idadeDias = idadeDias % 365;
+
+        int mes = idadeDias / 30;
+        idadeDias = idadeDias % 30;
+
+        int dias = idadeDias / 1;
+
+        Console.WriteLine($"{ano} ano(s)\n{mes} mes(es)\n{dias} dia(s)");
+        
+        Console.ReadKey();
+    }
+}
